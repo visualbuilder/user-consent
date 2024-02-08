ee2940d0b021037051d3b6c96a7e953d81a71dae
akshaybokaderanium <akshaybokaderanium@users.noreply.github.com>
2024-02-08 09:21:06 +0000

    Fix styling

--- tests/AdminPanelProvider.php
+++ tests/AdminPanelProvider.php
@@ -2,19 +2,19 @@
 
 namespace Visualbuilder\FilamentUserConsent\Tests;
 
+use Filament\Http\Middleware\Authenticate;
+use Filament\Http\Middleware\DisableBladeIconComponents;
+use Filament\Http\Middleware\DispatchServingFilamentEvent;
 use Filament\Panel;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
-use Filament\Http\Middleware\Authenticate;
-use Illuminate\Session\Middleware\StartSession;
+use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\AuthenticateSession;
+use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
-use Filament\Http\Middleware\DisableBladeIconComponents;
-use Filament\Http\Middleware\DispatchServingFilamentEvent;
-use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
-use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Visualbuilder\FilamentUserConsent\Resources\ConsentOptionResource;
 
 class AdminPanelProvider extends PanelProvider
--- tests/ResourcesTest.php
+++ tests/ResourcesTest.php
@@ -1,7 +1,8 @@
 <?php
 
-use function Pest\Laravel\get;
 use Visualbuilder\FilamentUserConsent\Resources\ConsentOptionResource;
+
+use function Pest\Laravel\get;
 
 it('can access user consent list page', function () {
     get(ConsentOptionResource::getUrl('index'))
