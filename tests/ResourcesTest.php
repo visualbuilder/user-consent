212cae47c33104e619c1202886b15cecd80c3282
akshaybokaderanium <akshaybokaderanium@users.noreply.github.com>
2024-02-13 07:10:50 +0000

    Fix styling

--- tests/ResourcesTest.php
+++ tests/ResourcesTest.php
@@ -1,13 +1,13 @@
 <?php
 
-use function Pest\Laravel\get;
-use function Pest\Livewire\livewire;
 use Visualbuilder\FilamentUserConsent\Models\ConsentOption;
 use Visualbuilder\FilamentUserConsent\Resources\ConsentOptionResource;
-
+use Visualbuilder\FilamentUserConsent\Resources\ConsentOptionResource\Pages\CreateConsentOption;
 use Visualbuilder\FilamentUserConsent\Resources\ConsentOptionResource\Pages\EditConsentOption;
 use Visualbuilder\FilamentUserConsent\Resources\ConsentOptionResource\Pages\ListConsentOptions;
-use Visualbuilder\FilamentUserConsent\Resources\ConsentOptionResource\Pages\CreateConsentOption;
+
+use function Pest\Laravel\get;
+use function Pest\Livewire\livewire;
 
 it('can access user consent list page', function () {
     get(ConsentOptionResource::getUrl('index'))
