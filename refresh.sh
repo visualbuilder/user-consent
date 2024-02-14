721e8f197cb057ba89806dd5b2f6b91db96a62ab
Akshay Bokade <akshay.bokade@ranium.in>
2024-02-14 18:23:25 +0530

    Refresh script implemented

--- refresh.sh
+++ refresh.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #For developer to recopy from source
+php artisan vendor:publish --tag=filament-user-consent-assets --force
 php artisan vendor:publish --tag=filament-user-consent-config --force
 php artisan vendor:publish --tag=filament-user-consent-migrations --force
 php artisan vendor:publish --tag=filament-user-consent-translations --force
