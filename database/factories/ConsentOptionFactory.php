022df148bd5ec56811f26a931a2a6c754fcff443
Lee Evans <lee@ekouk.com>
2024-02-25 21:39:40 +0000

    Update factory to include new fields addtional_info and fields

--- database/factories/ConsentOptionFactory.php
+++ database/factories/ConsentOptionFactory.php
@@ -15,17 +15,19 @@ class ConsentOptionFactory extends Factory
         $title = $this->faker->words(3, true);
 
         return [
-            'key' => Str::slug($title),
-            'version' => $this->faker->randomNumber(),
-            'title' => $title,
-            'label' => 'Tick here to accept the terms',
-            'text' => $this->faker->paragraph,
-            'is_mandatory' => 1,
-            'is_current' => 1,
+            'key'               => Str::slug($title),
+            'version'           => $this->faker->randomNumber(),
+            'title'             => $title,
+            'label'             => 'Tick here to accept the terms',
+            'text'              => $this->faker->paragraph,
+            'is_mandatory'      => 1,
+            'is_current'        => 1,
+            'additional_info'   => false,
+            'fields'            => [],
             'force_user_update' => 1,
-            'enabled' => 1,
-            'models' => config('filament-user-consent.models'),
-            'published_at' => now(),
+            'enabled'           => 1,
+            'models'            => config('filament-user-consent.models'),
+            'published_at'      => now(),
         ];
     }
 }
