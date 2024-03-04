51ae56f6d7452eca1ae69afd32139f9bd3925846
R. Aravindan <aravind.ram@ranium.in>
2024-03-04 15:01:35 +0530

    validate & dynamic form fild generations

--- tests/RequestTest.php
+++ tests/RequestTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use Visualbuilder\FilamentUserConsent\Livewire\ConsentOptionFormBuilder;
 use Visualbuilder\FilamentUserConsent\Models\ConsentOption;
 use Visualbuilder\FilamentUserConsent\Tests\Seeders\ConsentOptionSeeder;
 
@@ -10,4 +11,70 @@ use function Pest\Livewire\livewire;
 it('can access user consent list page', function () {
     $this->seed(ConsentOptionSeeder::class);
     get(route('consent-option-request'))->assertSuccessful();
+});
+
+
+it('can generate dynamic form fields', function() {
+    $this->seed(ConsentOptionSeeder::class);
+    $collections = auth()->user()->outstandingConsents();
+
+    $livewireComponent = livewire(ConsentOptionFormBuilder::class);
+    foreach($collections as $consentOption){
+        $livewireComponent->assertFormFieldExists("consents.$consentOption->id");
+
+        if((int)$consentOption->additional_info === 1) {
+            foreach ($consentOption->fields as $field) {
+                $livewireComponent->assertFormFieldExists("consents_info.$consentOption->id.{$field['name']}");
+            }
+        }
+    }
+    $livewireComponent->call('submit');
+    
+});
+
+it('validate mandatory user consents', function() {
+    $this->seed(ConsentOptionSeeder::class);
+    $collections = auth()->user()->outstandingConsents();
+    $fieldValidation = [];
+    foreach($collections as $consentOption){
+        if($consentOption->is_mandatory) {
+            $fieldValidation["consents.$consentOption->id"] = 'required';
+        }
+        if((int)$consentOption->additional_info === 1) {
+            foreach ($consentOption->fields as $field) {
+                if((bool)$field['required']) {
+                    $fieldValidation["consents_info.$consentOption->id.{$field['name']}"] = "required";    
+                }
+            }
+        }
+    }
+
+    livewire(ConsentOptionFormBuilder::class)
+        ->call('submit')
+        ->assertHasFormErrors()
+        ->assertHasFormErrors($fieldValidation);
+});
+
+
+it('can fill and save consents', function() {
+    $this->seed(ConsentOptionSeeder::class);
+    $collections = auth()->user()->outstandingConsents();
+    $fieldValidation = [];
+    foreach($collections as $consentOption){
+        if($consentOption->is_mandatory) {
+            $fieldValidation["consents.$consentOption->id"] = 'required';
+        }
+        if((int)$consentOption->additional_info === 1) {
+            foreach ($consentOption->fields as $field) {
+                if((bool)$field['required']) {
+                    $fieldValidation["consents_info.$consentOption->id.{$field['name']}"] = "required";    
+                }
+            }
+        }
+    }
+
+    livewire(ConsentOptionFormBuilder::class)
+        ->call('submit')
+        ->assertHasFormErrors()
+        ->assertHasFormErrors($fieldValidation);
 });
