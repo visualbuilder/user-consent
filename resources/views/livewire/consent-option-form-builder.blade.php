9bdfd4c8399c49ed48ea8002dea69666a2648921
R. Aravindan <aravind.ram@ranium.in>
2024-04-25 13:50:15 +0530

    set loader for consent form submit

--- resources/views/livewire/consent-option-form-builder.blade.php
+++ resources/views/livewire/consent-option-form-builder.blade.php
@@ -2,8 +2,11 @@
     <form wire:submit.prevent="submit">
         {{ $this->form }}
 
-        <div class="mt-3">
-            <x-filament::button icon="heroicon-m-sparkles" type="submit">
+        <div class="mt-3 text-end">
+            <x-filament::button type="submit" wire:loading>
+                <div class="flex"><x-filament::loading-indicator class="h-5 w-5 mx-3"/> Submitting consents...</div>
+            </x-filament::button>
+            <x-filament::button icon="heroicon-m-sparkles" type="submit" wire:loading.remove>
                 Submit Consents
             </x-filament::button>
         </div>
