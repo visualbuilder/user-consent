5f8ff7f19b5d9a9db710f8ec171a3854921bc9dd
Abdulbaki Suraj <codelikesuraj@gmail.com>
2026-01-13 10:21:12 +0100

    fix: create consent question action not showing in relation manager

--- src/Resources/ConsentOptionResource/RelationManagers/ConsentOptionQuestionsRelationManager.php
+++ src/Resources/ConsentOptionResource/RelationManagers/ConsentOptionQuestionsRelationManager.php
@@ -2,8 +2,6 @@
 
 namespace Visualbuilder\FilamentUserConsent\Resources\ConsentOptionResource\RelationManagers;
 
-use Closure;
-use Filament\Actions\BulkActionGroup;
 use Filament\Actions\CreateAction;
 use Filament\Actions\DeleteAction;
 use Filament\Actions\DeleteBulkAction;
@@ -16,7 +14,6 @@ use Filament\Schemas\Components\Utilities\Get;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Tables;
 use Filament\Tables\Table;
-
 use Illuminate\Support\Str;
 use Visualbuilder\FilamentTinyEditor\TinyEditor;
 
@@ -28,70 +25,75 @@ class ConsentOptionQuestionsRelationManager extends RelationManager
     {
         return $schema
             ->components([
-                Section::make()->components([
-                    Forms\Components\Select::make('component')
-                        ->required()
-                        ->options(config('filament-user-consent.components'))
-                        ->live()
-                        ->searchable(),
-                    Forms\Components\TextInput::make('name')
-                        ->required(fn(Get $get) => $get('component') !== 'placeholder')
-                        ->regex('/^[a-z_]+$/')
-                        ->live(true)
-                        ->hint('Alpha & underscore only allowed. (Ex: some_name)')
-                        ->maxLength(191)
-                        //->formatStateUsing(fn (string $state): string => Str::snake($state))
-                        ->afterStateUpdated(function ($state, $set) {
-                            $snake = Str::snake($state);
-                            // Update the name field's state to the snake case version of the input
-                            $set('name', $snake);
-                        }),
-                    Forms\Components\TextInput::make('label')
-                        ->nullable()
-                        ->maxLength(255),
-                    Forms\Components\Select::make('default_user_column')
-                        ->options(config('filament-user-consent.autofill_columns'))
-                        ->searchable(),
-                    Forms\Components\TextInput::make('sort')
-                        ->required()
-                        ->numeric(),
-                    Forms\Components\Toggle::make('required')
-                        ->inlineLabel(false)
-                        ->inline(false)
-                        ->visible(fn(Get $get) => $get('component') !== 'placeholder'),
-                ])->columns(3),
-                Section::make()->components([
-                    Repeater::make('options')
-                        ->relationship()
-                        ->schema([
-                            Forms\Components\TextInput::make('value')
-                                ->numeric()
-                                ->required(),
-                            Forms\Components\TextInput::make('text')
-                                ->required(),
-                            Forms\Components\TextInput::make('additional_info_label')
-                                ->label('Additional info label')
-                                ->required(fn(Get $get) => $get('additional_info')),
-                            Forms\Components\Select::make('additional_info_default_column')
-                                ->options(config('filament-user-consent.autofill_columns'))
-                                ->searchable(),
-                            Forms\Components\Toggle::make('additional_info')
-                                ->label('Additional info required?')
-                                ->inline(true)
-                                ->live()
-                                ->required(),
-                        ])
-                        ->reorderable(true)
-                        ->orderColumn('sort')
-                        ->columns(2)
-                ])->visible(fn (Get $get) => in_array($get('component'), ['likert', 'select', 'radio', 'check'])),
-                Section::make()->components([
-                    TinyEditor::make('content')
-                        ->label("HTML Content")
-                        ->profile('default')
-                        ->columnSpanFull()
-                ])->visible(fn (Get $get) => in_array($get('component'), ['placeholder']))
-
+                Section::make()
+                    ->columns(3)
+                    ->components([
+                        Forms\Components\Select::make('component')
+                            ->required()
+                            ->options(config('filament-user-consent.components'))
+                            ->live()
+                            ->searchable(),
+                        Forms\Components\TextInput::make('name')
+                            ->required(fn(Get $get) => $get('component') !== 'placeholder')
+                            ->regex('/^[a-z_]+$/')
+                            ->live(true)
+                            ->hint('Alpha & underscore only allowed. (Ex: some_name)')
+                            ->maxLength(191)
+                            //->formatStateUsing(fn (string $state): string => Str::snake($state))
+                            ->afterStateUpdated(function ($state, $set) {
+                                $snake = Str::snake($state);
+                                // Update the name field's state to the snake case version of the input
+                                $set('name', $snake);
+                            }),
+                        Forms\Components\TextInput::make('label')
+                            ->nullable()
+                            ->maxLength(255),
+                        Forms\Components\Select::make('default_user_column')
+                            ->options(config('filament-user-consent.autofill_columns'))
+                            ->searchable(),
+                        Forms\Components\TextInput::make('sort')
+                            ->required()
+                            ->numeric(),
+                        Forms\Components\Toggle::make('required')
+                            ->inlineLabel(false)
+                            ->inline(false)
+                            ->visible(fn(Get $get) => $get('component') !== 'placeholder')
+                    ]),
+                Section::make()
+                    ->visible(fn (Get $get) => in_array($get('component'), ['likert', 'select', 'radio', 'check']))
+                    ->components([
+                        Repeater::make('options')
+                            ->relationship()
+                            ->schema([
+                                Forms\Components\TextInput::make('value')
+                                    ->numeric()
+                                    ->required(),
+                                Forms\Components\TextInput::make('text')
+                                    ->required(),
+                                Forms\Components\TextInput::make('additional_info_label')
+                                    ->label('Additional info label')
+                                    ->required(fn(Get $get) => $get('additional_info')),
+                                Forms\Components\Select::make('additional_info_default_column')
+                                    ->options(config('filament-user-consent.autofill_columns'))
+                                    ->searchable(),
+                                Forms\Components\Toggle::make('additional_info')
+                                    ->label('Additional info required?')
+                                    ->inline(true)
+                                    ->live()
+                                    ->required(),
+                            ])
+                            ->reorderable(true)
+                            ->orderColumn('sort')
+                            ->columns(2)
+                    ]),
+                Section::make()
+                    ->visible(fn (Get $get) => in_array($get('component'), ['placeholder']))
+                    ->components([
+                        TinyEditor::make('content')
+                            ->label("HTML Content")
+                            ->profile('default')
+                            ->columnSpanFull()
+                    ])
             ]);
     }
 
@@ -106,20 +108,15 @@ class ConsentOptionQuestionsRelationManager extends RelationManager
                 Tables\Columns\TextColumn::make('sort'),
                 Tables\Columns\IconColumn::make('required'),
             ])
-            ->filters([
-                //
-            ])
             ->headerActions([
                 CreateAction::make(),
             ])
-            ->actions([
+            ->recordActions([
                 EditAction::make(),
                 DeleteAction::make(),
             ])
-            ->headerActions([
-                BulkActionGroup::make([
-                    DeleteBulkAction::make(),
-                ]),
+            ->toolbarActions([
+                DeleteBulkAction::make(),
             ]);
     }
 }
